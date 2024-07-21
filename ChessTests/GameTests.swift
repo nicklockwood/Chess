463bfa63cc1b0ba3636f9c65b5745f1c69579311
Nick Lockwood <nick@charcoaldesign.co.uk>
2024-07-21 08:42:01 +0100

    Fix pawn promotion not being recorded in history

--- ChessTests/GameTests.swift
+++ ChessTests/GameTests.swift
@@ -10,6 +10,34 @@
 import XCTest
 
 final class GameTests: XCTestCase {
+    // MARK: History
+
+    func testPawnPromotionRecordedInHistory() {
+        var game = Game(board: Board(pieces: [
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            ["WP0", nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+            [nil, nil, nil, nil, nil, nil, nil, nil],
+        ]))
+        game.makeMove(.init(from: .init(x: 0, y: 1), to: .init(x: 0, y: 0)))
+        game.promotePiece(at: .init(x: 0, y: 0), to: .queen)
+        XCTAssertEqual(game.board.piece(at: .init(x: 0, y: 0))?.type, .queen)
+        XCTAssertEqual(game.history.count, 1)
+        game.makeMove(.init(from: .init(x: 0, y: 0), to: .init(x: 1, y: 0)))
+        game.undo()
+        XCTAssertEqual(game.board.piece(at: .init(x: 0, y: 0))?.type, .queen)
+        game.undo()
+        XCTAssertNil(game.board.piece(at: .init(x: 0, y: 0)))
+        XCTAssertEqual(game.board.piece(at: .init(x: 0, y: 1))?.type, .pawn)
+        XCTAssertEqual(game.history.count, 0)
+    }
+
+    // MARK: End conditions
+
     func testOneBishopIsInsufficientMaterial() {
         let game = Game(board: Board(pieces: [
             [nil, nil, nil, nil, "BK4", "BB5", nil, nil],
